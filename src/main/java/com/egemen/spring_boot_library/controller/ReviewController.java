a05a7c3df9ac5c6700e21f9dd31f608fa631faf4
Egemen Sener <egemensener98@gmail.com>
2024-07-21 09:56:44 +0300

    review added and posted to db

---
+++ src/main/java/com/egemen/spring_boot_library/controller/ReviewController.java
@@ -0,0 +1,28 @@
+package com.egemen.spring_boot_library.controller;
+
+import com.egemen.spring_boot_library.requestmodels.ReviewRequest;
+import com.egemen.spring_boot_library.services.ReviewService;
+import com.egemen.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader("Authorization") String token, @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if(userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
---
+++ src/main/java/com/egemen/spring_boot_library/services/ReviewService.java
@@ -0,0 +1,46 @@
+package com.egemen.spring_boot_library.services;
+
+import com.egemen.spring_boot_library.dao.BookRepository;
+import com.egemen.spring_boot_library.dao.ReviewRepository;
+import com.egemen.spring_boot_library.entity.Review;
+import com.egemen.spring_boot_library.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+public class ReviewService {
+
+    private final BookRepository bookRepository;
+
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if (validateReview != null) {
+            throw new Exception("Review already created");
+        }
+
+        Review review = new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+        if (reviewRequest.getReviewDescription().isPresent()) {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Object::toString
+            ).orElse(null));
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+
+
+}
