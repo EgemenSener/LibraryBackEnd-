eb6df9d9051bf769562b92fe0122d33a5af1c71a
Egemen Sener <egemensener98@gmail.com>
2024-08-04 00:34:07 +0300

    deployment.bat düzenlendi war ve libler deployment klasörü altına atılıyor

--- deployment.bat
+++ deployment.bat
@@ -1,6 +1,8 @@
 @echo off
-REM JAR dosyalarını sileceğiniz klasörün yolu
-SET JAR_DIR=C:\Users\EGEMEN\CODING\Library\lib
+REM Klasor yollarını ayarlayın
+SET BASE_DIR=C:\Users\EGEMEN\CODING\Library\Deployment
+SET JAR_DIR=%BASE_DIR%\lib
+SET WAR_DIR=%BASE_DIR%\war
 
 REM Belirttiğiniz klasördeki tüm dosyaları sil
 echo Siliniyor: %JAR_DIR%\*.jar
@@ -26,5 +28,15 @@ IF ERRORLEVEL 1 (
     exit /b 1
 )
 
+REM Oluşan WAR dosyasını war klasörüne taşı
+echo WAR dosyasi tasiniyor...
+move /Y target\*.war %WAR_DIR%
+
+REM Eğer herhangi bir hata olursa, script burada durur
+IF ERRORLEVEL 1 (
+    echo WAR dosyasinin tasinmasi basarisiz oldu.
+    exit /b 1
+)
+
 echo Islem basariyla tamamlandi.
-exit
+exit /b 1
