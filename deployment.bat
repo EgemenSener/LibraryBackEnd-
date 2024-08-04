11e0cdadfb9a58e1178d88469dfd578c78728e23
Egemen Sener <egemensener98@gmail.com>
2024-08-04 20:42:57 +0300

    farklı jarları bulup klasörleyip ve da zipleyip asıl war ile beraber zipliyoruz ama packages'e atıyoruz!

--- deployment.bat
+++ deployment.bat
@@ -2,9 +2,21 @@
 echo ***** BY EGEMEN SENER *****
 REM Klasor yollarını ayarlayın
 SET BASE_DIR=C:\Users\EGEMEN\CODING\Library\Deployment
+SET PACKAGES_DIR=%BASE_DIR%\packages
 SET JAR_DIR=%BASE_DIR%\lib
 SET TEMP_JAR_DIR=%BASE_DIR%\temp_lib
 SET WAR_DIR=%BASE_DIR%\war
+
+REM War klasörünün içeriğini temizle
+echo WAR klasörünün içeriği temizleniyor...
+if exist %WAR_DIR%\* (
+    del /Q %WAR_DIR%\*
+)
+
+REM Virtualwarlib klasörünü temizle
+if exist %WAR_DIR%\virtualwarlib (
+    rmdir /S /Q %WAR_DIR%\virtualwarlib
+)
 
 REM Temp klasörü oluştur veya var olanı temizle
 if exist %TEMP_JAR_DIR% (
@@ -17,12 +29,12 @@ echo lib klasöründeki dosyalar temp_lib'e taşınıyor...
 move /Y %JAR_DIR%\*.jar %TEMP_JAR_DIR%
 
 REM Maven Wrapper kullanarak clean install işlemi başlat
-echo Maven clean install başlatiliyor...
+echo Maven clean install başlatılıyor...
 call mvnw clean install
 
 REM Eğer herhangi bir hata olursa, script burada durur
 IF ERRORLEVEL 1 (
-    echo Maven clean install basarisiz oldu.
+    echo Maven clean install başarısız oldu.
     exit /b 1
 )
 
@@ -32,21 +44,41 @@ call mvnw dependency:copy-dependencies -DoutputDirectory=%JAR_DIR% -DincludeScop
 
 REM Eğer herhangi bir hata olursa, script burada durur
 IF ERRORLEVEL 1 (
-    echo Bagimliliklarin kopyalanmasi basarisiz oldu.
+    echo Bağımlılıkların kopyalanması başarısız oldu.
     exit /b 1
 )
 
 REM Temp klasör ile lib klasörü arasındaki farkları bul ve WAR klasörüne kopyala
 echo Farklı JAR dosyaları tespit ediliyor ve WAR klasörüne taşınıyor...
+set newJarFound=false
 for %%f in (%JAR_DIR%\*.jar) do (
     if not exist "%TEMP_JAR_DIR%\%%~nxf" (
         copy "%%f" "%WAR_DIR%"
+        set newJarFound=true
     )
+)
+
+REM Yeni eklenen JAR dosyaları varsa, virtualwarlib klasörünü oluştur ve dosyaları buraya taşı
+if %newJarFound% == true (
+    mkdir %WAR_DIR%\virtualwarlib
+    move /Y %WAR_DIR%\*.jar %WAR_DIR%\virtualwarlib
+
+    REM Virtualwarlib klasörünü zipleyin
+    echo Virtualwarlib klasörü zipleniyor...
+    powershell Compress-Archive -Path "%WAR_DIR%\virtualwarlib" -DestinationPath "%WAR_DIR%\virtualwarlib.zip"
+
+    REM Virtualwarlib klasörünü silin
+    rd /s /q %WAR_DIR%\virtualwarlib
 )
 
 REM Oluşan WAR dosyasını war klasörüne taşı
 echo WAR dosyası taşınıyor...
-move /Y target\*.war %WAR_DIR%\library.war
+if exist target\*.war (
+    move /Y target\*.war %WAR_DIR%\library.war
+) else (
+    echo Hata: WAR dosyası bulunamadı.
+    exit /b 1
+)
 
 REM Eğer herhangi bir hata olursa, script burada durur
 IF ERRORLEVEL 1 (
@@ -65,13 +97,14 @@ set minute=%time:~3,2%
 REM Tarih ve saat formatını ayarla
 set formattedDate=%year%%month%%day%-%hour%%minute%
 
-REM Farklı JAR dosyalarını ve WAR dosyasını zipleyin
-echo WAR dosyası ve yeni JAR'lar zipleniyor...
-powershell Compress-Archive -Path "%WAR_DIR%\library.war", "%WAR_DIR%\*.jar" -DestinationPath "%WAR_DIR%\%formattedDate%-library-war.zip"
-
-REM WAR dosyasını ve fark dosyalarını sil
-del /Q %WAR_DIR%\library.war
-del /Q %WAR_DIR%\*.jar
+REM WAR dosyasını ve varsa virtualwarlib.zip dosyasını zipleyin
+if exist %WAR_DIR%\virtualwarlib.zip (
+    echo WAR dosyası ve virtualwarlib.zip dosyası zipleniyor...
+    powershell Compress-Archive -Path "%WAR_DIR%\library.war", "%WAR_DIR%\virtualwarlib.zip" -DestinationPath "%PACKAGES_DIR%\%formattedDate%-library-war.zip"
+) else (
+    echo Sadece WAR dosyası zipleniyor...
+    powershell Compress-Archive -Path "%WAR_DIR%\library.war" -DestinationPath "%PACKAGES_DIR%\%formattedDate%-library-war.zip"
+)
 
 REM Temp klasörü temizle
 rmdir /S /Q %TEMP_JAR_DIR%
@@ -82,5 +115,5 @@ IF ERRORLEVEL 1 (
     exit /b 1
 )
 
-echo ***** Deployment Dosyasi Hazir *****
+echo ***** Deployment Dosyası Hazır *****
 exit /b 0
